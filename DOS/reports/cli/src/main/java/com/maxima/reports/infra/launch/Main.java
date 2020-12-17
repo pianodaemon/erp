fad97fecb5feec7775e62a2793effd170d08e614
Koji Kabuto <koji@mazinger.com>
2020-12-16 20:30:17 -0600

    godinez: command line approach is retaken
    Signed-off-by: Koji Kabuto <koji@mazinger.com>

---
+++ DOS/reports/cli/src/main/java/com/maxima/reports/infra/launch/Main.java
@@ -0,0 +1,9 @@
+package com.maxima.reports.infra.launch;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello world");
+    }
+}
