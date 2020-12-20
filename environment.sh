cc70bac5046737aab5acdce76fc75477890733f3
Koji Kabuto <koji@mazinger.com>
2020-12-20 10:34:06 -0600

    CI-000: Timestamps for builds

--- environment.sh
+++ environment.sh
@@ -1,7 +1,9 @@
 STAGE=${STAGE:-"$HOME"}
 
-export BINDIR=$STAGE/build
-export RELDIR=$STAGE/release/$(date +%s)
+TIME_STAMP_BUILD=$(date +%s)
+
+export BINDIR="$STAGE/$TIME_STAMP_BUILD/out"
+export RELDIR="$STAGE/$TIME_STAMP_BUILD/release"
 
 [ -d $BINDIR ] || mkdir -p $BINDIR
 [ -d $RELDIR ] || mkdir -p $RELDIR
