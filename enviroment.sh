02ab76e4e3da3acec96bae2bb9bc985c4f25f324
Koji Kabuto <koji@mazinger.com>
2020-12-20 00:07:01 -0600

    CI-000: workspace is not incorporated

--- enviroment.sh
+++ enviroment.sh
@@ -1,7 +1,7 @@
-WORKSPACE=${WORKSPACE:-"$HOME"}
+STAGE=${STAGE:-"$HOME"}
 
-export BINDIR=$WORKSPACE/build
-export RELDIR=$WORKSPACE/release/$(date +%s)
+export BINDIR=$STAGE/build
+export RELDIR=$STAGE/release/$(date +%s)
 
 [ -d $BINDIR ] || mkdir -p $BINDIR
 [ -d $RELDIR ] || mkdir -p $RELDIR
