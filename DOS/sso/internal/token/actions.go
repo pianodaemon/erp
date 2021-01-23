ebc982e6ecd68b5e9e8cf37ef8c6263c18013a44
Edwin Plauchu <j4nusx@yahoo.com>
2021-01-23 10:10:10 -0600

    sso: reduction of token claims
    token generation is no longer containing authorities
    
    Signed-off-by: Edwin Plauchu <j4nusx@yahoo.com>

--- DOS/sso/internal/token/actions.go
+++ DOS/sso/internal/token/actions.go
@@ -58,7 +58,7 @@ func ExtractFromReq(publicKey *rsa.PublicKey, req *http.Request, checkSignMethod
 }
 
 // Generates a token along with its claims by signing with private key
-func Generate(privateKey *rsa.PrivateKey, expirationDelta int, userUID string, userAuths string) (string, error) {
+func Generate(privateKey *rsa.PrivateKey, expirationDelta int, userUID string) (string, error) {
 
 	token := jwt.New(jwt.SigningMethodRS512)
 
@@ -66,7 +66,6 @@ func Generate(privateKey *rsa.PrivateKey, expirationDelta int, userUID string, u
 		"exp":         time.Now().Add(time.Hour * time.Duration(expirationDelta)).Unix(),
 		"iat":         time.Now().Unix(),
 		"sub":         userUID,
-		"authorities": userAuths,
 	}
 
 	tokenString, err := token.SignedString(privateKey)
