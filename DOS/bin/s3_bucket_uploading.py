673f3676625424c38652931e1eaab35f1efd8bd9
Edwin Plauchu <j4nusx@yahoo.com>
2021-02-08 19:45:34 -0600

    A fancy dump cloud. It has never been probed.
    Signed-off-by: Edwin Plauchu <j4nusx@yahoo.com>

--- DOS/bin/s3_bucket_uploading.py
+++ DOS/bin/s3_bucket_uploading.py
@@ -1,24 +1,55 @@
+#!/usr/bin/env python3
+
+import sys
 import docker
 import boto3
 import gzip
 from datetime import datetime
 
-db_container = 'rdbms_dos'
-database     = 'erp'
-bucket_name  = 'medica-dumps'
-s3_obj_name  = 'rrm-dump-' + datetime.now().strftime('%Y-%m-%d') + '.sql.gz'
 
-# Getting the database dump
-client = docker.from_env()
+class PgDumpCloud(object):
 
-container = client.containers.get(db_container)
-exec_code, output = container.exec_run('pg_dump -h localhost -U postgres --create {}'.format(database))
+    _TRANSITIVE_CMD_FMT = """pg_dump -h localhost -U postgres --create {}"""
+    _DUMP_FILENAME_FMT = """{}-dump-{}.sql.gz"""
 
-if exec_code != 0:
-    raise Exception("PANIC!!!: " + exec_code)
+    def __call__(self, dumper, placer):
 
+        # Getting the database dump
+        exec_code, data = dumper(self._tcmd)
 
-s3 = boto3.resource('s3')
+        if exec_code != 0:
+            raise Exception("PANIC!!!: " + exec_code)
 
-# Compressing and uploading
-s3.Bucket(bucket_name).put_object(Key=s3_obj_name, Body=gzip.compress(output))
+        # Compressing and uploading
+        placer(data, self._sthree_fname)
+
+    @classmethod
+    def bucketize(cls, db_container, db_instance, target_bucket, prefix):
+        dumper = cls._gear_up_dump(db_container)
+        placer = cls._placement(target_bucket)
+        ic = cls(dumper, placer)
+        ic._sthree_fname = cls._DUMP_FILENAME_FMT.format(db_instance, datetime.now().strftime('%Y-%m-%d'))
+        ic._tcmd = cls._TRANSITIVE_CMD_FMT.format(db_instance)
+        return ic
+
+    @classmethod
+    def _placement(cls, target_bucket)
+        sthree_res = boto3.resource('s3')
+        return lambda data, fname: sthree_res.Bucket(target_bucket).put_object(Key=fname, Body=gzip.compress(data))
+
+    @classmethod
+    def _gear_up_dump(cls, db_container):
+        client = docker.from_env()
+        container = client.containers.get(db_container)
+        return lambda tcmd: container.exec_run(tcmd)
+
+if __name__ == "__main__":
+
+    pgdc = PgDumpCloud.bucketize('rdbms_dos', 'erp', 'medica-dumps')
+
+    try:
+        pgdc()
+    except KeyboardInterrupt:
+        print('Exiting')
+    except:
+        sys.exit(1)
