10da028a5598d06c4dc8dc0a5d479392166fa2c7
Edwin Plauchu <j4nusx@yahoo.com>
2021-02-11 21:18:38 -0600

    Fix typo
    Signed-off-by: Edwin Plauchu <j4nusx@yahoo.com>

--- DOS/cfdiengine/service/pac/fake.py
+++ DOS/cfdiengine/service/pac/fake.py
@@ -4,16 +4,7 @@ from pac.adapter import Adapter
 __complement = '''
     <cfdi:Complemento>
 
-        <tfd:TimbreFiscalDigital xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sat.gob.mx/Timbre
-FiscalDigital http://www.sat.gob.mx/sitio_internet/cfd/TimbreFiscalDigital/TimbreFiscalDigitalv11.xsd" FechaTimbrado="2019-08-0
-1T18:38:21" NoCertificadoSAT="00001000000413990493" RfcProvCertif="PFE140312IW8" SelloCFD="enytZjgJcp22ofUhqHV1dUQ3eJNnscpJPqt1
-KpbMdcpnAK4tqOK0rtADAfheRycXO7CMUCxnfTSvE++Jfuq7Yg25Oj7nc4y4tVHf4tPhMn4VBPeK55xInqLxEKrjgTiGbEoozhktw14wU4q27SE+WkQe7icyOLVHJid
-ru++mU1bY5G5yjJOtLSdChMgiqR7R0D4LMI+B9bieljH1n8dXXRw1I7jg92xPHE2toqOwJigtlG11kwP4AZu6LfQQJY+WVc6nSJgTtIbzbNGN3q5uF/r4Uf1dv/y7Is
-/b9a1KFLSXVqkSOR3Et2eaPd6cJ6nyTjctFGGyyvbqs7qdrt+Cbw==" SelloSAT="ItheHhejC5UJvzeJAhcCnqDMfO9882WU4ktQLRTSqfq9IbN7+GON4aAvn7/yz
-eDqo+bIdpBK8RsAfJ5BXNehAanmvbaLVoUSuGqnLH8eT2VwXtIUKKMG4npErBEps97Fv9MqINIC0FsHQDIwR4G7avsvvgle90S7NHFigaX50RO/Zf5vtVtUFcHoZx3O
-YYsM8O3JKj52sG9TvMSPC0D18PxTryjxvTEDBRyDmjVOXUg/Kh8sXFAaRUnieYjm+CGup+kIR8vAWWFKw3PPhc9gQfoms+2Oe2GirfzTNBELmfju4DSXSv6OHXeOyEE
-ZSb6WFPRFo0Xc53l5+UWR6X/2yg==" UUID="eae49b31-7af2-405a-9928-327d8cf3f3e2" Version="1.1" xmlns:tfd="http://www.sat.gob.mx/Timbr
-eFiscalDigital" />
+        <tfd:TimbreFiscalDigital xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sat.gob.mx/TimbreFiscalDigital http://www.sat.gob.mx/sitio_internet/cfd/TimbreFiscalDigital/TimbreFiscalDigitalv11.xsd" FechaTimbrado="2019-08-01T18:38:21" NoCertificadoSAT="00001000000413990493" RfcProvCertif="PFE140312IW8" SelloCFD="enytZjgJcp22ofUhqHV1dUQ3eJNnscpJPqt1KpbMdcpnAK4tqOK0rtADAfheRycXO7CMUCxnfTSvE++Jfuq7Yg25Oj7nc4y4tVHf4tPhMn4VBPeK55xInqLxEKrjgTiGbEoozhktw14wU4q27SE+WkQe7icyOLVHJidru++mU1bY5G5yjJOtLSdChMgiqR7R0D4LMI+B9bieljH1n8dXXRw1I7jg92xPHE2toqOwJigtlG11kwP4AZu6LfQQJY+WVc6nSJgTtIbzbNGN3q5uF/r4Uf1dv/y7Is/b9a1KFLSXVqkSOR3Et2eaPd6cJ6nyTjctFGGyyvbqs7qdrt+Cbw==" SelloSAT="ItheHhejC5UJvzeJAhcCnqDMfO9882WU4ktQLRTSqfq9IbN7+GON4aAvn7/yzeDqo+bIdpBK8RsAfJ5BXNehAanmvbaLVoUSuGqnLH8eT2VwXtIUKKMG4npErBEps97Fv9MqINIC0FsHQDIwR4G7avsvvgle90S7NHFigaX50RO/Zf5vtVtUFcHoZx3OYYsM8O3JKj52sG9TvMSPC0D18PxTryjxvTEDBRyDmjVOXUg/Kh8sXFAaRUnieYjm+CGup+kIR8vAWWFKw3PPhc9gQfoms+2Oe2GirfzTNBELmfju4DSXSv6OHXeOyEEZSb6WFPRFo0Xc53l5+UWR6X/2yg==" UUID="eae49b31-7af2-405a-9928-327d8cf3f3e2" Version="1.1" xmlns:tfd="http://www.sat.gob.mx/TimbreFiscalDigital" />
 
     </cfdi:Complemento>
 </cfdi:Comprobante>
