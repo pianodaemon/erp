ea15313201a202e3e39269c85b4e1b7bc5625136
Edwin Plauchu <j4nusx@yahoo.com>
2021-02-16 08:19:19 -0600

    Pulling as string a bucket object
    Signed-off-by: Edwin Plauchu <j4nusx@yahoo.com>

--- DOS/cfdiengine/service/misc/sthreeops.py
+++ DOS/cfdiengine/service/misc/sthreeops.py
@@ -16,6 +16,9 @@ class SthreeOps(object):
     def __init__(self, **kwargs):
         self.session = boto3.Session(**kwargs)
 
+    def __del__(self):
+        pass
+
     @staticmethod
     def _bucketize(session, target_bucket, fname):
         """Having a session it uploads the file into the s3 bucket"""
@@ -32,8 +35,25 @@ class SthreeOps(object):
             emsg = "file {} could not be placed at {} : ({})"
             raise Exception(emsg.format(fname, target_bucket, e))
 
+    @staticmethod
+    def _retrieve_str(session, target_bucket, item_name):
+        """Having a session it retrieves an utf-8 string"""
+        client = session.client("s3")
+        try:
+            obj = client.get_object(Bucket=target_bucket, Key=item_name)
+            return obj['Body'].read().decode('utf-8')
+        except ClientError as e:
+            emsg = "file {} could not be retrieved from {} : ({})"
+            raise Exception(emsg.format(fname, target_bucket, e))
+
     @classmethod
     def placement(cls, target_bucket, fname):
         """It places a regular file into a s3 bucket"""
         ic = cls(**_BOTO_KWARGS)
-        cls._placement(ic.session, target_bucket, fname)
+        cls._bucketize(ic.session, target_bucket, fname)
+
+    @classmethod
+    def pull_text(cls, target_bucket, fname):
+        """It retrieves an utf-8 string"""
+        ic = cls(**_BOTO_KWARGS)
+        cls._retrieve_str(ic.session, target_bucket, fname)
