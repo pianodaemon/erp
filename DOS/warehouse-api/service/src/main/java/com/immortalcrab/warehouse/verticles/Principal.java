854083025076219947e72f31f40f4b9e56195fa8
Edwin Plauchu <j4nusx@yahoo.com>
2021-02-23 20:17:49 -0600

    warehouse-api: Fleshing out the principal verticle
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

--- DOS/warehouse-api/service/src/main/java/com/immortalcrab/warehouse/verticles/Principal.java
+++ DOS/warehouse-api/service/src/main/java/com/immortalcrab/warehouse/verticles/Principal.java
@@ -1,32 +1,62 @@
 package com.immortalcrab.warehouse.verticles;
 
+import io.vertx.config.ConfigRetriever;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class Principal extends AbstractVerticle {
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
+    private Future<String> spinUpHttpServer() {
 
-    vertx.deployVerticle(new StaticFileServer());
+        Promise<String> promise = Promise.promise();
 
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        logger.info("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
-  }
+        vertx.createHttpServer().requestHandler(req -> {
+            req.response()
+                    .putHeader("content-type", "text/plain")
+                    .end("Hello from Vert.x!");
+        }).listen(8888, http -> {
+            if (http.succeeded()) {
+                promise.complete("HTTP server started on port 8888");
+            } else {
+                promise.fail(http.cause());
+            }
+        });
 
-  private final Logger logger = LoggerFactory.getLogger(Principal.class);
+        return promise.future();
+    }
 
+    private void gearUpVerticles(ConfigRetriever retriever) {
+
+        DeploymentOptions opts = new DeploymentOptions()
+                .setInstances(Principal.WORKER_INSTANCES)
+                .setWorker(true);
+
+        vertx.deployVerticle(SyncDbBridge.class.getName(), opts);
+
+        vertx.deployVerticle(StaticFileServer.class.getName());
+    }
+
+    @Override
+    public void start() throws Exception {
+
+        Future<String> future = this.spinUpHttpServer();
+        ConfigRetriever retriever = ConfigRetriever.create(vertx);
+
+        this.gearUpVerticles(retriever);
+
+        future.onComplete(ar -> {
+            if (ar.failed()) {
+                logger.warn("Something bad happened: {}", ar.cause().toString());
+            } else {
+                logger.info("Result: " + ar.result());
+            }
+        });
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(Principal.class);
+    public static final int WORKER_INSTANCES = 2;
 }
---
+++ DOS/warehouse-api/service/src/main/java/com/immortalcrab/warehouse/verticles/SyncDbBridge.java
@@ -0,0 +1,15 @@
+package com.immortalcrab.warehouse.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SyncDbBridge extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(SyncDbBridge.class);
+}
