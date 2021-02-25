8d1794d3203e503e442078a831e6840df130a222
Edwin Plauchu <j4nusx@yahoo.com>
2021-02-25 10:47:27 -0600

    warehouse-api: sql code to request existance
    This should return later existance as per the presentation.
    
    Signed-off-by: Edward Nygma <j4nusx@yahoo.com>

--- DOS/warehouse-api/service/src/main/java/com/immortalcrab/warehouse/persistence/WareHouseMgmt.java
+++ DOS/warehouse-api/service/src/main/java/com/immortalcrab/warehouse/persistence/WareHouseMgmt.java
@@ -13,6 +13,24 @@ public class WareHouseMgmt {
     int counter;
     private BasicDataSource ds;
 
+    void requestExistancePerPresentation(final Integer productId,
+            final Integer presentationId, final Integer warehouseId) {
+
+        MessageFormat mf = new MessageFormat(
+                String.format("SELECT exis, decimales FROM ( "
+                        + " SELECT ( "
+                        + " inv_exi_pres.inicial::double precision + inv_exi_pres.entradas::double precision "
+                        + " - inv_exi_pres.salidas::double precision - inv_exi_pres.reservado::double precision "
+                        + ") AS exis, inv_prod_unidades.decimales FROM inv_exi_pres JOIN inv_prod ON inv_prod.id=inv_exi_pres.inv_prod_id "
+                        + " JOIN inv_prod_unidades ON inv_prod_unidades.id=inv_prod.unidad_id WHERE inv_exi_pres.inv_alm_id=? "
+                        + " AND inv_exi_pres.inv_prod_id=? AND inv_exi_pres.inv_prod_presentacion_id=?)AS sbt WHERE exis>0 ")
+        );
+
+        String sqlQuery = mf.format(new Object[]{warehouseId, productId, presentationId});
+
+        this.logger.info(sqlQuery);
+    }
+
     private BasicDataSource configureConnectionPool() {
 
         Map<String, String> envConf = System.getenv();
